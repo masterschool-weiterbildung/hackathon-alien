3d1c28a2a7e1938ad8c80e7510cf1c8bfcd0b3a5
amirdehestani <amirmo800@gmail.com>
2025-01-22 00:02:32 +0100

    Add functions for fetching ISS and UFO data to the process_selection function.

--- menu.py
+++ menu.py
@@ -1,14 +1,14 @@
-import request_iss_data
+
 from api import send_sms_number
+from request_iss_data import iss_tracker
+from scrape_ufo_sighting import fetch_ufo_data, display_ufo_data
 
 
 # Function to show the menu to the user
-# Only this version work
-
 def show_menu(number):
     menu = ("Welcome, Space Traveler!\n\nPlease choose an option:\n\n"
             "1. **Diplomatic Tips & Tricks**\n- Learn how to navigate space diplomacy with humor!\n\n"
-            "2. **Klingon Translator**\n- Ready to speak like a Klingon? Translate your message here.\n\n"
+            "2. **UFO Data**\n- Stay updated on the latest UFO sightings and mysterious space phenomena!\n\n"
             "3. **API ISS Information**\n- Get real-time data about the International Space Station (ISS).\n\n"
             "4. **Exit ('I am OK')**\n- Leave the space adventure. We hope to see you again soon!")
 
@@ -34,22 +34,35 @@ def process_selection(number, selection):
         diplomatic_tips(number)
 
     elif selection == "2":
-        send_sms_number([{number: "You selected: Klingon Translator."}])
-
+        send_sms_number([{number: "You selected: UFO Data."}])
+        ufo_data = fetch_ufo_data()
+        print("\nHide! Latest UFO sightings: \n")
+        display_ufo_data(ufo_data)
 
     elif selection == "3":
         send_sms_number([{number: "You selected: API ISS Information."}])
+        iss_data = iss_tracker()  # Get ISS information
+
+        if "error" in iss_data:
+            send_sms_number([{number: iss_data["error"]}])
+        else:
+
+            message = (
+                f"The ISS is currently over {iss_data['location_name']} at Latitude: {iss_data['latitude']}, "
+                f"Longitude: {iss_data['longitude']}"
+            )
+            send_sms_number([{number: message}])
 
 
     elif selection == "4":
         send_sms_number([{number: "You selected: Exit. Goodbye!"}])
     else:
-        send_sms_number([{number: "❌ Invalid selection. Please try again."}])
+        send_sms_number([{number: "Invalid selection. Please try again."}])
 
 
 def main():
     show_menu("4915735156801")
-    diplomatic_tips("4915735156801")
+    # diplomatic_tips("4915735156801")
 
 
 if __name__ == "__main__":
