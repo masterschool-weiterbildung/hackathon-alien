2340d2e7f20a8739bc3988b45acb7968c0d5d06b
amirdehestani <amirmo800@gmail.com>
2025-01-21 16:50:32 +0100

    reformat menu and tips

--- menu.py
+++ menu.py
@@ -2,6 +2,7 @@ from api import send_sms_number
 
 
 # Function to show the menu to the user
+
 def show_menu(number):
     menu = "Welcome, Space Traveler!\n\n1. Diplomacy Tips\n2.Klingon Translator\n3.ISS Data\n4.Exit"
 
@@ -9,8 +10,8 @@ def show_menu(number):
 #
 def diplomatic_tips(number):
     tips = ("Diplomatic Tips:\n1.Don’t comment on glowing eyes.\n2.Avoid touching glowing buttons."
-           "\n3.Distract with shiny objects.\n4.Don’t ask about alien food.\n5.Sit if offered, unless inflatable."
-           "\n6.Skip weather talk—it’s touchy")
+            "\n3.Distract with shiny objects.\n4.Don’t ask about alien food.\n5.Sit if offered, unless inflatable."
+            "\n6.Skip weather talk—it’s touchy")
 
     send_sms_number([{number: tips}])
 
@@ -33,3 +34,9 @@ def process_selection(number, selection):
     else:
         send_sms_number([{number: "❌ Invalid selection. Please try again."}])
 
+def main():
+    # show_menu("4915735156801")
+    diplomatic_tips("4915735156801")
+
+if __name__ == "__main__":
+    main()
